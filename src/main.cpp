a51e778bff390a8ec0db2762d9a2d3aac4376be8
Thuan Bach <ThuanBach@uregina.ca>
2021-11-19 20:56:52 +0700

    Init C++ Project

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : CS700Assignment5.cpp
+// Author      : Thuan Bach
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!d
+	return 0;
+}
